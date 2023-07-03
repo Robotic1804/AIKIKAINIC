7781948c8135ce7b5758309f73620c10d5642c98
Norman Navarro <93636275+Robotic1804@users.noreply.github.com>
2023-07-02 21:27:37 -0600

    update modals

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ModalService {
+  register(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
 
   private visible = false;
 
